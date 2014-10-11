77127517d09111aed9c35d7aeee3256a144d354b
Sujay Khandekar <sskhandek@gmail.com>
2014-10-11 13:14:53 -0700

    Initial commit

---
+++ MoveScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		speed = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (speed == 0) {
+			transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
+
+				}
+				else if (transform.position.y < 1)
+			transform.position =new Vector3(transform.position.x,  transform.position.y + speed * Time.deltaTime, transform.position.z);
+		else
+			this.gameObject.SetActive(false);
+	}
+}
---
+++ Project Files/Assets/PersonBehaviorScript.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class PersonBehaviorScript : MonoBehaviour {
+	 	
+	private GameObject[] people;
+	private List<int> deleted;
+	private MicControlC mic;
+	int counter;
+	int pause_counter;
+	int babble_counter;
+	
+	void Start () {
+		people = GameObject.FindGameObjectsWithTag("Audience");
+		deleted = new List<int> ();
+		mic = GetComponent<MicControlC> ();
+		counter = 0;
+		pause_counter = 0;
+		babble_counter = 0;
+	}
+		
+	void Update () {
+
+		if (counter % 150 == 0) {
+
+			float loud = mic.loudness;
+
+			// too soft
+			if (loud < 150.0) {
+				reduceAudience ();
+				pause_counter++;
+			} else {
+				pause_counter = 0;
+			}
+
+			if (pause_counter == 4) {
+				reduceAudience();
+				reduceAudience();
+			}
+
+			// too loud
+			if (loud > 400.0) {
+				increaseAudience ();
+				increaseAudience ();
+			}
+			if (loud > 300.0) {
+				increaseAudience();
+				increaseAudience();
+				babble_counter++;
+			} else {
+				babble_counter = 0;
+			}
+			if (loud > 200.0) {
+				increaseAudience();
+				increaseAudience();	
+			}
+
+			if (babble_counter == 8) {
+				reduceAudience ();
+				reduceAudience ();
+			}
+
+		}
+
+		counter++;
+	}
+
+	void reduceAudience () {
+		if (deleted.Count < people.Length ) {
+			int decreaseHere = Random.Range (0, people.Length);
+			while (deleted.Contains (decreaseHere)) {
+				decreaseHere = Random.Range (0, people.Length);
+			}
+			people[decreaseHere].animation.Play();
+			people[decreaseHere].GetComponent<MoveScript>().speed = 2f;
+			//people [decreaseHere].SetActive (false);
+			deleted.Add (decreaseHere);
+		}
+	}
+
+	void increaseAudience () {
+
+		int index = Random.Range (0, deleted.Count);
+		if (deleted.Count == 0)
+						return;
+		int increaseHere = deleted[index];
+		people[increaseHere].SetActive(true);
+		people [increaseHere].GetComponent<MoveScript> ().speed = 0f;
+		deleted.RemoveAt (index);
+	}
+}
